201d58f90fed769375421a97ee3f2a4ef5786ff6
Isobel Smith <isobel@bridesmaid.org>
2016-06-28 17:49:33 +0100

    simplified code

--- session_id/sessionid.py
+++ session_id/sessionid.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
-@app.route('/agents')
-def agents():
-    return render_template('agents.html')
+@app.route('/session_id')
+def session_id():
+   return render_template('session_id.html')
 
