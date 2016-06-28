3b50281a120fa87191cf4f0d0e4d12c61e8271fe
Isobel Smith <isobel@bridesmaid.org>
2016-06-28 17:06:40 +0100

    added comments

--- session_continuation/session_continuation.py
+++ session_continuation/session_continuation.py
@@ -4,17 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+# renders the html index page
    return render_template('index.html')
 
 @app.route('/enter_form')
 def enter_form():
+# renders the html file
    return render_template('enter_form.html')
 
 
 @app.route('/save_form', methods = ['POST', 'GET'])
 def save_form():
+#renders the html file
    return render_template('save_form.html')
 
 @app.route('/reciept')
 def reciept():
+#renders the html file
    return render_template('reciept.html')
