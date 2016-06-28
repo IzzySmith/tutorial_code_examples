4781f1e7fbfef46b4b511fd8b0cb2094bb60afb1
Isobel Smith <isobel@bridesmaid.org>
2016-06-28 16:36:33 +0100

    updated to remove pythonic structure- needs revision and comments added

--- session_continuation/session_continuation.py
+++ session_continuation/session_continuation.py
@@ -13,28 +13,8 @@ def enter_form():
 
 @app.route('/save_form', methods = ['POST', 'GET'])
 def save_form():
-   if request.method == 'POST':  
-      the_username = request.form['user_name']
-      the_surname = request.form['user_surname']
-      the_card = request.form['card']
-      the_address = request.form['address']
-
-      resp = make_response(render_template('save_form.html'))
-      resp.set_cookie('username', the_username)
-      resp.set_cookie('surname', the_surname)
-      resp.set_cookie('card', the_card)
-      resp.set_cookie('address', the_address)
-
-      return resp
- 
-   else:
-      return render_template('save_form.html')
+   return render_template('save_form.html')
 
 @app.route('/reciept')
 def reciept():
-   username = request.cookies.get('username')
-   surname = request.cookies.get('surname')
-   card = request.cookies.get('card')
-   address = request.cookies.get('address')
-   return '<table><tr><td>Name: </td><td> '+username+' </td></tr><tr><td>Surname: </td><td> '+surname+' </td></tr><tr><td> Card Details: </td><td> '+card+'</td></tr><tr><td> Address: </td><td> '+address+' </td></tr></table>'
-
+   return render_template('reciept.html')
